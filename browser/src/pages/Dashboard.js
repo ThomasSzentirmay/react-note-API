0abef258fc79cb10dedb6aa8b44e9e37e2ae434e
Thomas Szentirmay <tlszent@outlook.com>
2023-08-10 04:19:18 +1000

    display notes on dashboard

--- browser/src/pages/Dashboard.js
+++ browser/src/pages/Dashboard.js
@@ -1,3 +1,5 @@
+
+
 function Dashboard(props) {
     return (
         <main className="dashboard">
@@ -6,7 +8,16 @@ function Dashboard(props) {
             <h3>Here are your saved notes</h3>
 
             <div className="notes">
-                <p>No notes have been added</p>
+                {!props.state.user.notes.length && <p>No notes have been added.</p>}
+
+                {props.state.user.notes.map(note => (
+                <div key={note._id} className="note">
+                    <h3>{note.text}</h3>
+                    <div className="row">
+                    <p>Added On: {note.createdAt}</p>
+                    </div>
+                </div>
+                ))}
             </div>
         </main>
     )
--- browser/src/pages/Landing.js
+++ browser/src/pages/Landing.js
@@ -1,15 +1,37 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 function Landing(props) {
-    return (
-        <main className="landing">
-            <h1 className="text-center">Welcome to the future in note taking!</h1>
+  const [notes, setNotes] = useState([]);
 
-            <h3>See what notes other are taking:</h3>
+  useEffect(() => {
+    axios.get('/api/notes')
+      .then(res => {
+        setNotes([...res.data.notes]);
+      });
+  }, []);
 
-            <div className="notes">
-                <p>No notes have been added</p>
+  return (
+    <main className="landing">
+      <h1 className="text-center">Welcome to the future in note taking!</h1>
+
+      <h3>See what notes others are taking:</h3>
+
+      <div className="notes">
+        {!notes.length && <p>No notes have been added.</p>}
+
+        {notes.map(note => (
+          <div key={note._id} className="note">
+            <h3>{note.text}</h3>
+            <div className="row">
+              <p>Added On: {note.createdAt}</p>
+              <p>Added By: {note.author.username}</p>
             </div>
-        </main>
-    )
+          </div>
+        ))}
+      </div>
+    </main>
+  )
 }
 
 export default Landing;
