b2d30bea69d65ce1263d85dffd63249fd1830f5f
Thomas Szentirmay <tlszent@outlook.com>
2023-08-10 04:30:12 +1000

    handle dashboard note submissions

--- browser/src/pages/Dashboard.js
+++ browser/src/pages/Dashboard.js
@@ -1,22 +1,56 @@
-
+import {useState} from 'react';
+import axios from 'axios';
 
 function Dashboard(props) {
+    const [formData, setFormData] = useState({
+        text: '',
+        user_id: props.state.user._id
+    });
+
+    const handleInputChange = e => {
+        setFormData({
+            ...formData,
+            text: e.target.value
+        });
+    }
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+
+        const user = await axios.post('/api/note', formData);
+
+        props.setState(oldState => ({
+            ...oldState,
+            user
+        }));
+
+        setFormData({
+            text: ''
+        });
+    }
+
     return (
         <main className="dashboard">
             <h1 className="text-center">Welcome, {props.state.user.username}!</h1>
 
-            <h3>Here are your saved notes</h3>
+            <form onSubmit={handleSubmit} className="column dashboard-form">
+                <h2 className="text-center">Create a note</h2>
+                <input value={formData.text} onChange={handleInputChange} type="text" placeholder="Enter your note text" />
+                <button>Submit</button>
+            </form>
+
+            <h3>Here are your saved notes:</h3>
 
             <div className="notes">
                 {!props.state.user.notes.length && <p>No notes have been added.</p>}
 
                 {props.state.user.notes.map(note => (
-                <div key={note._id} className="note">
-                    <h3>{note.text}</h3>
-                    <div className="row">
-                    <p>Added On: {note.createdAt}</p>
+                    <div key={note._id} className="note column">
+                        <h3>{note.text}</h3>
+                        <div className="column">
+                            <p>Added On: {note.createdAt}</p>
+                        </div>
                     </div>
-                </div>
                 ))}
             </div>
         </main>
